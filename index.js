68496e864dd77008426b194113639a937f5a1683
pallab99 <kitkatpallab@gmail.com>
2023-06-01 12:26:07 +0600

    Node JS basic

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+import { readFileSync } from "fs";
+import http from "http";
+
+const home = readFileSync("./home.html");
+const about = readFileSync("./about.html");
+const contact = readFileSync("./contact.html");
+const pageNotFound = readFileSync("./pageNotFound.html");
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/about") {
+    res.end(about);
+  } else if (req.url == "/contact") {
+    res.end(contact);
+  } else if (req.url == "/") {
+    res.end(home);
+  } else {
+    res.end(pageNotFound);
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server is connected");
+});
